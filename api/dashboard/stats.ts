954fd3e26f50f613db43fb79713d4a16a43b1fc7
Ramya V <ramyasahana2@gmail.com>
2026-06-26 13:43:29 +0530

    Move db helper to lib and fix imports

--- api/dashboard/stats.ts
+++ api/dashboard/stats.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { db } from '../db';
+import { db } from '../../lib/db';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'GET') {
